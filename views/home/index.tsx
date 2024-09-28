1fd534b3309c01ae6969fed921bad4e8b2bf5ee3
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-09-28 02:27:09 +0100

    :sparkles: feat: add sound (#1)

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,30 +1,59 @@
 import { Motion } from '@interest-protocol/ui-kit';
-import { Img } from '@stylin.js/elements';
-import { FC } from 'react';
+import { Audio, AudioElementProps, Img } from '@stylin.js/elements';
+import { FC, useEffect, useRef, useState } from 'react';
 
 import { Layout } from '@/components';
 import Shadow from '@/components/shadow';
 
-const Home: FC = () => (
-  <Layout>
-    <Motion
-      initial={{ scale: 1 }}
-      animate={{ scale: 1.1 }}
-      transition={{ repeat: Infinity, repeatType: 'reverse' }}
-    >
-      <Img src="/logo.webp" />
-    </Motion>
-    <Motion
-      top="-40%"
-      right="0%"
-      position="absolute"
-      initial={{ opacity: 1 }}
-      animate={{ opacity: [1, 0.5, 1] }}
-      transition={{ duration: 5, repeat: Infinity, ease: 'easeInOut' }}
-    >
-      <Shadow />
-    </Motion>
-  </Layout>
-);
+const Home: FC = () => {
+  const audioRef = useRef<AudioElementProps>(null);
+  const [firstPlay, setFirstPlay] = useState(true);
+
+  const playSound = () => {
+    const element = audioRef.current as HTMLAudioElement;
+    if (element && element.paused && firstPlay) {
+      setFirstPlay(false);
+      element.play();
+    }
+  };
+
+  const toggleSound = () => {
+    const element = audioRef.current as HTMLAudioElement;
+    if (element) {
+      if (element.paused) element.play();
+      else element.pause();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', playSound);
+    return () => window.removeEventListener('mousemove', playSound);
+  }, []);
+
+  return (
+    <Layout>
+      <Motion
+        zIndex="0"
+        top="-40%"
+        right="0%"
+        position="absolute"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: [1, 0.5, 1] }}
+        transition={{ duration: 5, repeat: Infinity, ease: 'easeInOut' }}
+      >
+        <Shadow />
+      </Motion>
+      <Motion
+        onClick={toggleSound}
+        initial={{ scale: 1 }}
+        animate={{ scale: 1.1 }}
+        transition={{ repeat: Infinity, repeatType: 'reverse' }}
+      >
+        <Img src="/logo.webp" />
+      </Motion>
+      <Audio loop src="/gg.mp3" ref={audioRef} />
+    </Layout>
+  );
+};
 
 export default Home;
