47b5cedfaab20b6002ffefa3ac193a093363c7b9
git-marcopitra <marcomorete73@hotmail.com>
2024-11-03 07:26:06 +0100

    :mag: feat: add SEO to GG page

--- components/seo/index.tsx
+++ components/seo/index.tsx
@@ -4,6 +4,14 @@ import { FC } from 'react';
 const SEO: FC = () => (
   <Head>
     <title>MEMEZ.GG</title>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@memezdotgg" />
+    <meta name="twitter:title" content="MEMEZ.GG" />
+    <meta name="twitter:description" content="Good Game" />
+    <meta name="twitter:image" content="/memezbanner.jpeg" />
+    <meta name="twitter:title" content="MEMEZ.GG" />
+    <meta name="twitter:image" content="/memezbanner.jpeg" />
+    <meta name="twitter:description" content="Good Game" />
     <link rel="icon" type="image/x-icon" href="/icon.webp" />
   </Head>
 );
